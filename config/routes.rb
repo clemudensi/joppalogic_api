e7eef11062d9a49a3d83af8e7925cd95563a75be
itzbernoulli <oyinloyeayodeji@gmail.com>
2017-07-22 19:51:34 +0000

    (strip twilio) twilio integration reversed

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,6 @@ Rails.application.routes.draw do
 		resources :users, only: [:index,:create,:update]
 		resources :parcels
 		resources :items, only: [:index]
-		resources :notifications do 
-			collection do
-				get :trigger_sms_alerts 
-			end
-		end
 		
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
