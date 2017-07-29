d33f7357ae1fa2132c8c07d4f103a97784776940
itzbernoulli <oyinloyeayodeji@gmail.com>
2017-07-29 13:27:43 +0000

    (ref) commented out the line to drop database table

--- db/migrate/20170728195944_create_user_roles.rb
+++ db/migrate/20170728195944_create_user_roles.rb
@@ -1,7 +1,7 @@
 class CreateUserRoles < ActiveRecord::Migration[5.0]
   def change
   	#be sure to remove this migration after it works
-  	drop_table :user_roles
+  	# drop_table :user_roles
 
     create_table :user_roles do |t|
       t.references :real_user, foreign_key: true
