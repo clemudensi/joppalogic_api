6c9ff05fcab66261c523a02c84ac3bd37d9bb1db
itzbernoulli <oyinloyeayodeji@gmail.com>
2017-07-30 23:37:57 +0000

    (ref) added a seed file with some csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,22 @@
 # # # 		email: "ayo@gmail.com",
 # # # 		password: "ayo@gmail.com"
 # # # 		)
-# Rate.delete_all
-# Courier.delete_all
+Rate.delete_all
+Courier.delete_all
 # Parcel.delete_all
 # # RealUser.delete_all
-# Area.delete_all
+Area.delete_all
 
-# courier1 = Courier.create(
-# 			name: "Jerry Express",
-# 			phone: "+233 (0) 265231422",
-# 			web_url: "http://www.jerry.com",
-# 			description: "Jerry delivers within Ghana",
-# 			active: true,
-# 			capabilities: ["International shipping","Local shipping"],
-# 			parameters: {username: "joppalogic",
-# 						password: "joppa+logic"}
-# 			)
+courier1 = Courier.create(
+			name: "Jerry Express",
+			phone: "+233 (0) 265231422",
+			web_url: "http://www.jerry.com",
+			description: "Jerry delivers within Ghana",
+			active: true,
+			capabilities: ["International shipping","Local shipping"],
+			parameters: {username: "joppalogic",
+						password: "joppa+logic"}
+			)
 
 # area1 = Area.create(
 # 	name: "East Legon",
@@ -98,3 +98,39 @@
 # # # Role.create(name: "logistics_admin")
 # # # Role.create(name: "logistics_staff")
 # # # Role.create(name: "logistics_super_admin")
+
+
+require 'csv'
+
+jerry_csv = File.read(Rails.root.join('lib', 'seeds', 'jerryexpress.csv'))
+area_csv = File.read(Rails.root.join('lib', 'seeds', 'Areas.csv'))
+parsed_area = CSV.parse(area_csv, :headers => true, :encoding => 'ISO-8859-1')
+parsed_jerry = CSV.parse(jerry_csv, :headers => true, :encoding => 'ISO-8859-1')
+
+parsed_area.each do |row|
+	a = Area.new
+	a.name = row['Area']
+	a.area_type = "neighbourhood"
+	a.region = "Accra"
+	a.country = "Ghana"
+	a.save
+	# puts "#{a.name} is saved"
+	# puts "#{a.id} is saved"
+  # puts row.to_hash
+end
+
+
+parsed_jerry.each do |j|
+		from_area = Area.find_by_slug(j['From'].strip.downcase.parameterize('-'))
+		to_area = Area.find_by_slug(j['To'].strip.downcase.parameterize('-'))
+		
+		r = Rate.new
+		r.from_area_id = from_area.id
+		r.to_area_id = to_area.id
+		r.courier_id = courier1.id
+		r.price = j['Price'].to_f
+		r.save
+	end
+puts "There are now #{Area.count} areas in the database"
+puts "There are now #{Rate.count} areas in the database"
+# puts csv_text
