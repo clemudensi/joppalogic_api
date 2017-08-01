34332f5c72dde46baa440512c24295153773c291
itzbernoulli <oyinloyeayodeji@gmail.com>
2017-08-01 22:07:29 +0000

    (ref) added the seed file for lme

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,89 +9,25 @@
 # # # 		email: "ayo@gmail.com",
 # # # 		password: "ayo@gmail.com"
 # # # 		)
-Rate.delete_all
-Courier.delete_all
-# Parcel.delete_all
-# # RealUser.delete_all
-Area.delete_all
+# Rate.delete_all
+# Courier.delete_all
+# # Parcel.delete_all
+# # # RealUser.delete_all
+# Area.delete_all
 
 courier1 = Courier.create(
 			name: "Jerry Express",
-			phone: "+233 (0) 265231422",
-			web_url: "http://www.jerry.com",
-			description: "Jerry delivers within Ghana",
+			phone: "+233 (0) 202020090",
+			image_url: "http://res.cloudinary.com/joppalogic/image/upload/v1501623650/lme_p08nr0.jpg",
+			web_url: "http://www.lme.com",
+			description: "LME delivers within Ghana",
 			active: true,
-			capabilities: ["International shipping","Local shipping"],
+			capabilities: ["Local shipping"],
 			parameters: {username: "joppalogic",
 						password: "joppa+logic"}
 			)
 
-# area1 = Area.create(
-# 	name: "East Legon",
-# 	area_type: "neighbourhood",
-# 	region: "accra",
-# 	country: "Ghana"
-# 	)
 
-# area2 = Area.create(
-# 	name: "Madina",
-# 	area_type: "neighbourhood",
-# 	region: "accra",
-# 	country: "Ghana"
-# 	)
-
-# area3 = Area.create(
-# 	name: "Osu",
-# 	area_type: "neighbourhood",
-# 	region: "accra",
-# 	country: "Ghana"
-# 	)
-
-
-# courier1.rates.create(
-# 			from_area_id: area1.id,
-# 			to_area_id: area1.id,
-# 			price: 5.00)
-
-# courier1.rates.create(
-# 			from_area_id: area1.id,
-# 			to_area_id: area2.id,
-# 			price: 7.00)
-
-# courier1.rates.create(
-# 			from_area_id: area1.id,
-# 			to_area_id: area3.id,
-# 			price: 6.00)
-
-# # courier1.rates.create(
-# # 			from_area: 1,
-# # 			to_area: 1a",
-# # 			price: 10.00)
-
-# # courier1.rates.create(
-# # 			from_area: 1,
-# # 			to_area: 1aha",
-# # 			price: 12.00)
-
-# # courier1.rates.create(
-# # 			from_area: 1,
-# # 			to_area: 1a",
-# # 			price: 20.00)
-
-# # courier1.rates.create(
-# # 			from_area: 1,
-# # 			to_area: 1",
-# # 			price: 5.00)
-
-# # courier1.rates.create(
-# # 			from_area: 1,
-# # 			to_area: 1",
-# # 			price: 8.00)
-
-# # courier1.rates.create(
-# # 			from_area: 1,
-# # 			to_area: 1ands",
-# # 			price: 5.00)
 
 # # # Role.create(name: "joppalogic")
 # # # Role.create(name: "logistics_super_admin")
@@ -102,22 +38,23 @@ courier1 = Courier.create(
 
 require 'csv'
 
-jerry_csv = File.read(Rails.root.join('lib', 'seeds', 'jerryexpress.csv'))
+# jerry_csv = File.read(Rails.root.join('lib', 'seeds', 'jerryexpress.csv'))
+jerry_csv = File.read(Rails.root.join('lib', 'seeds', 'lme.csv'))
 area_csv = File.read(Rails.root.join('lib', 'seeds', 'Areas.csv'))
 parsed_area = CSV.parse(area_csv, :headers => true, :encoding => 'ISO-8859-1')
 parsed_jerry = CSV.parse(jerry_csv, :headers => true, :encoding => 'ISO-8859-1')
 
-parsed_area.each do |row|
-	a = Area.new
-	a.name = row['Area']
-	a.area_type = "neighbourhood"
-	a.region = "Accra"
-	a.country = "Ghana"
-	a.save
-	# puts "#{a.name} is saved"
-	# puts "#{a.id} is saved"
-  # puts row.to_hash
-end
+# parsed_area.each do |row|
+# 	a = Area.new
+# 	a.name = row['Area']
+# 	a.area_type = "neighbourhood"
+# 	a.region = "Accra"
+# 	a.country = "Ghana"
+# 	a.save
+# 	# puts "#{a.name} is saved"
+# 	# puts "#{a.id} is saved"
+#   # puts row.to_hash
+# end
 
 
 parsed_jerry.each do |j|
